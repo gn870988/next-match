2f6258daeaffc01d67df9d0e898ca8326a57a9da
Vincent.yu <gn870988@gmail.com>
2024-06-23 22:19:03 +0800

    End of section 5

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,3 +1,20 @@
-export default function ListsPage() {
-  return <div>ListsPage12</div>;
+import {
+  fetchCurrentUserLikeIds,
+  fetchLikedMembers,
+} from "../actions/likeActions";
+import ListsTab from "./ListsTab";
+
+export default async function ListsPage({
+  searchParams,
+}: {
+  searchParams: { type: string };
+}) {
+  const likeIds = await fetchCurrentUserLikeIds();
+  const members = await fetchLikedMembers(searchParams.type);
+
+  return (
+    <div>
+      <ListsTab members={members} likeIds={likeIds} />
+    </div>
+  );
 }
