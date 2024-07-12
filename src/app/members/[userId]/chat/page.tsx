49a00ed65568da8d82abdb6805bf3d3cbe865ff9
Vincent.yu <gn870988@gmail.com>
2024-07-12 16:03:37 +0800

    End of section 7

--- src/app/members/[userId]/chat/page.tsx
+++ src/app/members/[userId]/chat/page.tsx
@@ -1,3 +1,34 @@
-export default function ChatPage() {
-  return <div>ChatPage</div>;
+import { getAuthUserId } from "@/app/actions/authActions";
+import { getMessageThread } from "@/app/actions/messageActions";
+import CardInnerWrapper from "@/components/CardInnerWrapper";
+import ChatForm from "./ChatForm";
+import MessageBox from "./MessageBox";
+
+export default async function ChatPage({
+  params,
+}: {
+  params: { userId: string };
+}) {
+  const userId = await getAuthUserId();
+  const messages = await getMessageThread(params.userId);
+
+  const body = (
+    <div>
+      {messages.length === 0 ? (
+        "No messages to display"
+      ) : (
+        <div>
+          {messages.map((message) => (
+            <MessageBox
+              key={message.id}
+              message={message}
+              currentUserId={userId}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+
+  return <CardInnerWrapper header="Chat" body={body} footer={<ChatForm />} />;
 }
